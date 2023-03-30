21074c0ee17b6b0d93cf886925d807d2ffdf830e
Eda Işıl İnal <119332810+Eda-Inal@users.noreply.github.com>
2023-03-30 09:31:18 +0300

    Update Input.jsx

--- Input.jsx
+++ Input.jsx
@@ -1,1 +1,27 @@
 
+import { useState } from "react";
+
+function Input(){
+    const [form, setForm] = useState({
+        name : "", surname :""
+    });
+ 
+    const onChangeInput = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+    return(
+        <div>
+          name <br />
+        <input name="name" value={form.name} onChange = {onChangeInput}/> <br />
+
+        surname <br />
+        <input name="surname" value={form.surname} onChange = {onChangeInput}/> <br />
+            
+        
+        
+
+        </div>
+    )
+}
+
+export default Input;
